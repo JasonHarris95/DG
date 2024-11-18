d557c050ab734dd3f860b3f901b082c990eb1751
Jason <wellnetstudio@gmail.com>
2024-11-18 15:07:39 +0300

    'first'

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views                     # . из этой же папки
 
 urlpatterns = [
-    path('', views.home, name='home'),               #путь, функция представления, имя пути
+    path('ru/', views.home_ru, name='home_ru'),               #путь, функция представления, имя пути
+    path('games_ru/', views.games_ru, name='games_ru'),
+    path('projects_ru/', views.projects_ru, name='projects_ru'),
+    path('contacts_ru/', views.contacts_ru, name='contacts_ru'),
+    path('en/', views.home, name='home'),
     path('games/', views.games, name='games'),
     path('projects/', views.projects, name='projects'),
     path('contacts/', views.contacts, name='contacts'),
--- main/views.py
+++ main/views.py
@@ -1,14 +1,36 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-menu = [{'title':'HOME', 'url_name':'home'},              #переменная-список, для передачи в словарь data меню сайта
+menu_ru = [{'title':'ГЛАВНАЯ', 'url_name':'home_ru'},        #переменная-список, для передачи в словарь data меню сайта
+        {'title':'ИГРЫ', 'url_name':'games_ru'},
+        {'title':'ДРУГИЕ ПРОЕКТЫ', 'url_name':'projects_ru'},
+        {'title':'КОНТАКТЫ', 'url_name':'contacts_ru'}]
+
+menu_en = [{'title':'HOME', 'url_name':'home'},              #переменная-список, для передачи в словарь data меню сайта
         {'title':'GAMES', 'url_name':'games'},
-        {'title':'PROJECTS', 'url_name':'projects'},
+        {'title':'OTHER PROJECTS', 'url_name':'projects'},
         {'title':'CONTACTS', 'url_name':'contacts'}]
+
+def home_ru(request):                                        #параметр запроса
+    data = {'title':'DreamG | Главная',
+            'menu':menu_ru}
+    return render(request, 'main/home_ru.html', context=data)
+
+def games_ru(request):
+    data = {'title': 'DreamG | Игры'}
+    return render(request, 'main/games_ru.html', context=data)
+
+def projects_ru(request):
+    data = {'title': 'DreamG | Другие проекты'}
+    return render(request, 'main/projects_ru.html', context=data)
+
+def contacts_ru(request):
+    data = {'title': 'DreamG | Контакты'}
+    return render(request, 'main/contacts_ru.html', context=data)
 
 def home(request):                                        #параметр запроса
     data = {'title':'DreamG | Home',
-            'menu':menu}
+            'menu':menu_en}
     return render(request, 'main/home.html', context=data)
 
 def games(request):
