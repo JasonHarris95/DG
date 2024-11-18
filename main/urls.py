ae17d73431a7092fbe1d01554e1977f758863bbd
Jason <wellnetstudio@gmail.com>
2024-11-18 17:18:22 +0300

    number_03

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views                     # . из этой же папки
 
 urlpatterns = [
-    path('ru/', views.home_ru, name='home_ru'),               #путь, функция представления, имя пути
+    path('', views.home_ru, name='home_ru'),               #путь, функция представления, имя пути
     path('games_ru/', views.games_ru, name='games_ru'),
     path('projects_ru/', views.projects_ru, name='projects_ru'),
     path('contacts_ru/', views.contacts_ru, name='contacts_ru'),
