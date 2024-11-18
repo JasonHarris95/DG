bfa97fb278b9495a5a9804a3d0b2c401b5b6c000
Jason <wellnetstudio@gmail.com>
2024-11-18 18:01:56 +0300

    number_05

--- DreamG/settings.py
+++ DreamG/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import SECRET_KEY, STATIC_ROOT
+from django.conf.global_settings import SECRET_KEY, STATIC_ROOT, DEBUG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
